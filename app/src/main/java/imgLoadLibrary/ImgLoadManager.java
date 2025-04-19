ca4e5479c4a8c8ab6ce284744681d41bde01fb31
Ivan Savitsky <isavitsky17@gmail.com>
2025-04-20 01:26:31 +0700

    change while(true) to observer pattern

--- app/src/main/java/imgLoadLibrary/ImgLoadManager.java
+++ app/src/main/java/imgLoadLibrary/ImgLoadManager.java
@@ -13,6 +13,9 @@ import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.lang.ref.WeakReference;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
@@ -21,16 +24,17 @@ import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 public class ImgLoadManager {
-    static ImgLoadManager singleton = null;
-    int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
-    int cacheSize = maxMemory / 8;
+    private static ImgLoadManager singleton = null;
+    private int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
+    private int cacheSize = maxMemory / 8;
     private ConcurrentHashMap<String, Boolean> urlSettedMap = new ConcurrentHashMap<>();
-    private final LruCache<String, Bitmap> urlToImgLoaderMapCache = new LruCache<String, Bitmap>(cacheSize) {
+    private final LruCache<String, Bitmap> urlToBitmapMapCache = new LruCache<String, Bitmap>(cacheSize) {
         @Override
         protected int sizeOf(String key, Bitmap bitmap) {
             return bitmap.getByteCount() / 1024;
         }
     };
+    private HashMap<String, List<ImageSetter>> urlToImageSetterMap = new HashMap<>();
 
     ExecutorService threadPoolExecutor = new ThreadPoolExecutor(
             10,
@@ -44,7 +48,7 @@ public class ImgLoadManager {
     }
 
     public void logCacheSize() {
-        Log.d("mydebug", "cache size: " + this.urlToImgLoaderMapCache.size());
+        Log.d("mydebug", "cache size: " + this.urlToBitmapMapCache.size());
     }
 
     public static synchronized ImgLoadManager get() {
@@ -58,7 +62,7 @@ public class ImgLoadManager {
         WeakReference<ImageView> imageViewRef = new WeakReference<>(imageView);
         Future future = threadPoolExecutor.submit(() -> {
             // Log.d("mydebug", Thread.currentThread().getName() + " load " );
-            getBitmapFromCacheOrDownload(url, imageView);
+            getBitmapFromCacheOrDownload(url, imageViewRef);
         });
 
         imageViewRef.get().addOnAttachStateChangeListener(new View.OnAttachStateChangeListener() {
@@ -72,14 +76,25 @@ public class ImgLoadManager {
                 if (future != null) {
                     future.cancel(true);
                 }
+//                urlToImageSetterMap.get(url).clear();
+//                urlToImageSetterMap.remove(url);
             }
         });
     }
 
-    private void getBitmapFromCacheOrDownload(String url, ImageView imageView) {
-        WeakReference<ImageView> imageViewRef = new WeakReference<>(imageView);
+    private void getBitmapFromCacheOrDownload(String url, WeakReference<ImageView> imageViewRef) {
+        Bitmap cached = urlToBitmapMapCache.get(url);
 
-        if (urlSettedMap.get(url) == null) {
+        if (urlToImageSetterMap.get(url) == null) {
+            urlToImageSetterMap.put(url, new ArrayList<>());
+        }
+        ImageSetter imageSetter = new ImageSetter(url, imageViewRef);
+        urlToImageSetterMap.get(url).add(imageSetter);
+
+        if (cached != null) {
+            Log.d("mydebug", "cache hit");
+            imageSetter.setImage(cached);
+        } else if (urlSettedMap.get(url) == null) {
             urlSettedMap.put(url, true);
             Log.d("mydebug", "UrlSetted");
 
@@ -88,26 +103,12 @@ public class ImgLoadManager {
                 Log.d("mydebug", Thread.currentThread().getName() + " network download: " + url);
 
                 if (bitmap != null) {
-                    urlToImgLoaderMapCache.put(url, bitmap.copy(bitmap.getConfig(), true));
+                    urlToBitmapMapCache.put(url, bitmap.copy(bitmap.getConfig(), true));
                     Log.d("mydebug", "putted");
+                    urlToImageSetterMap.get(url).forEach(IS -> IS.setImage(bitmap));
                 }
-                imageViewRef.get().post(() -> {
-                    if (bitmap != null) {
-                        imageViewRef.get().setImageBitmap(bitmap);
-                    }
-                });
             } catch (IOException e) {
                 e.printStackTrace();
-            }
-        } else {
-            while (true) {
-                if (urlToImgLoaderMapCache.get(url) != null) {
-                    Log.d("mydebug", "цикл прервался");
-                    imageViewRef.get().post(() -> {
-                        imageViewRef.get().setImageBitmap(urlToImgLoaderMapCache.get(url));
-                    });
-                    break;
-                }
             }
         }
     }
